9f4520d1e6954384079b81234d26852805b5f8ff
zrcool <akshatanurag1998@gmail.com>
2020-05-03 18:21:10 +0530

    cv published

--- contact.php
+++
@@ -1,6 +0,0 @@
-$name=$_POST['name'];
-$email=$_POST['email'];
-$phone=$_POST['phone'];
-$message=$_POST['message'];
-mail("akshatanurag1998@gmail.com","Tremor","Name: $name \n\n Email: $email \n\n Phone: $phone \n\n Message: $message \n\n");
-echo "Thank You, We will get back to you ASAP!";
